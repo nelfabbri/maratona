3339b0ecf94265a3d4be9cec65ae8c38e70f2bd4
nelfabbri <nelfabbri@gmail.com>
2024-09-06 19:40:26 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ function pesquisar() {
     // Função responsável por realizar a pesquisa e exibir os resultados na página.
     // Obtém a seção HTML onde os resultados serão exibidos.
     let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
     console.log(section); // Verifica se a seção foi encontrada no DOM.
   
+    if (!campoPesquisa) {
+      section.innerHTML = "<p>Nada foi encontrado. Digite um nome ou algo.</p>"
+      return
+    }
+    campoPesquisa = campoPesquisa.toLowerCase()
     let resultados = "";
-  
+   
+    let titulo ="";
+    let descricao = "";
+    let tags = "";
+
     // Itera sobre os dados da pesquisa e constrói o HTML para cada resultado.
     // A variável 'dado' representa cada item da lista de resultados.
     for (let dado of dados) {
+       titulo = dado.titulo.toLowerCase()
+       descricao = dado.descricao.toLowerCase()
+       tags = dado.tags.toLowerCase()
+     if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
       resultados += `
         <div class="item-resultado">
           <h2>
@@ -18,8 +33,13 @@ function pesquisar() {
           <a href=${dado.link} target="_blank">Mais Informações</a>
         </div>
       `;
+     }
+       
     }
-  
+     if (!resultados) {
+         resultados = "<p>Nada foi encontrado</p>"
+
+     }
     // Atribui o HTML gerado à seção de resultados.
     section.innerHTML = resultados;
   }
--- dados.js
+++ dados.js
@@ -2,62 +2,74 @@ let dados = [
     {
         titulo: "Marilson Gomes dos Santos",
         descricao: "Marilson Gomes dos Santos é um ícone do atletismo brasileiro, especialmente nas provas de longa distância. Sua carreira, marcada pela dedicação e talento, o levou a conquistar títulos importantes como duas vitórias na Maratona de Nova York, tornando-se o primeiro sul-americano a alcançar esse feito. Além disso, suas três vitórias na tradicional Corrida de São Silvestre e suas diversas medalhas em Jogos Pan-Americanos consolidaram seu lugar na história do esporte nacional. Marilson, que também detém recordes sul-americanos nos 5.000m e 10.000m, é um exemplo de atleta completo e inspirador para futuras geraçõesRayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-        link: "https://en.wikipedia.org/wiki/Mar%C3%ADlson_Gomes_dos_Santos"
+        link: "https://en.wikipedia.org/wiki/Mar%C3%ADlson_Gomes_dos_Santos",
+        tags: "olimpíada nova york"
     },
     {
         titulo: "Vanderlei Cordeiro de Lima",
         descricao: "Vanderlei Cordeiro de Lima é um maratonista brasileiro conhecido por sua  história emocionante nos Jogos Olímpicos de Atenas 2004. Líder da prova, foi empurrado por um invasor, mas conseguiu terminar a corrida em terceiro lugar, demonstrando grande espírito esportivo.",
-        link: "https://pt.wikipedia.org/wiki/Vanderlei_Cordeiro_de_Lima"
+        link: "https://pt.wikipedia.org/wiki/Vanderlei_Cordeiro_de_Lima",
+        tags: "padre olimpíada "
     },
     {
         titulo: "Daniel Nascimento",
         descricao: "Daniel Nascimento é um maratonista brasileiro que vem se destacando no cenário internacional. Com um tempo de 2h05min16s, ele possui o recorde brasileiro da maratona. Conhecido  por sua força e resistência, Daniel é uma das grandes promessas do atletismo brasileir.",
-        link: "https://pt.wikipedia.org/wiki/Daniel_Nascimento_(atleta)"
+        link: "https://pt.wikipedia.org/wiki/Daniel_Nascimento_(atleta)",
+         tags: "record"
     },
     {
         titulo: "Adriana Aparecida da Silva",
         descricao: "Adriana é uma das maiores maratonistas brasileiras da história. Ela detém o recorde sul-americano da maratona e representou o Brasil em diversas competições internacionais, incluindo os Jogos Olímpicos.",
-        link: "https://pt.wikipedia.org/wiki/Adriana_Aparecida_da_Silva"
+        link: "https://pt.wikipedia.org/wiki/Adriana_Aparecida_da_Silva",
+         tags: ""
     },
     {
         titulo: "Carmem de Oliveira",
         descricao: "Carmem foi uma pioneira da maratona feminina no Brasil. Ela competiu em alto nível nas décadas de 1980 e 1990, participando de Jogos Olímpicos e conquistando medalhas em Jogos Pan-Americanos.",
-        link: "https://www.corridaecia.com.br/carmem-de-oliveira-a-primeira-a-vencer-barreiras-na-maratona/"
+        link: "https://www.corridaecia.com.br/carmem-de-oliveira-a-primeira-a-vencer-barreiras-na-maratona/",
+         tags: ""
     },
     {
         titulo: "Solonei Rocha",
         descricao: "Solonei foi um dos principais nomes da maratona brasileira nos anos 2000. Ele representou o Brasil em Jogos Olímpicos e conquistou pódios importantes em maratonas internacionais.",
-        link: "https://esportes.estadao.com.br/noticias/geral,aos-40-anos-solonei-da-silva-ainda-sonha-alto-na-maratona,70001756075"
+        link: "https://esportes.estadao.com.br/noticias/geral,aos-40-anos-solonei-da-silva-ainda-sonha-alto-na-maratona,70001756075",
+         tags: ""
     },
     {
         titulo: "Frank Caldeira",
         descricao: "Frank Caldeira é um maratonista brasileiro com resultados expressivos em provas internacionais. Ele representou o Brasil nos Jogos Olímpicos de Londres 2012.",
-        link: "https://www.cob.org.br/pt/cob/time-brasil/atletas/frank-caldeira/"
+        link: "https://www.cob.org.br/pt/cob/time-brasil/atletas/frank-caldeira/",
+         tags: ""
     },
     {
         titulo: "Edson Amaro",
         descricao: "Edson Amaro é um maratonista brasileiro que se destacou nas décadas de 1990 e 2000, com participações em Jogos Olímpicos e  vitórias em importantes maratonas.",
-        link: "https://www.olimpiadatododia.com.br/atletismo/166826-edson-amaro-relembra-sua-trajetoria-no-atletismo/" 
+        link: "https://www.olimpiadatododia.com.br/atletismo/166826-edson-amaro-relembra-sua-trajetoria-no-atletismo/" ,
+         tags: ""
     },
     {
         titulo: "Roselaine Benevides",
         descricao: "Roselaine Benevides é uma maratonista brasileira com participações em Jogos Pan-Americanos. Ela é conhecida por sua garra e determinação nas provas.",
-        link: "https://www.brasildefato.com.br/2015/07/24/de-empregada-domestica-a-atleta-olimpica-maratonista-supera-barreiras-e-faz-historia"
+        link: "https://www.brasildefato.com.br/2015/07/24/de-empregada-domestica-a-atleta-olimpica-maratonista-supera-barreiras-e-faz-historia",
+         tags: ""
     },
     {
         titulo: "Valdir de Oliveira",
         descricao: "Valdir de Oliveira, conhecido como 'Seo Valdir', é um exemplo de superação e paixão pelo esporte. Ele começou a correr maratonas aos 40 anos e se tornou um ícone da corrida de rua no Brasil.",
-        link: "https://www.youtube.com/watch?v=s8-b9gD57qQ" 
+        link: "https://www.youtube.com/watch?v=s8-b9gD57qQ" ,
+         tags: ""
     },
     {
         titulo: "Sueli Silva",
         descricao: "Sueli Silva é uma maratonista brasileira que representou o país em Jogos Olímpicos e conquistou medalhas em Jogos Pan-Americanos, sendo uma das principais atletas do Brasil na modalidade nos anos 2000.",
-        link: "https://www.iaaf.org/athletes/brazil/sueli-pinto-silva-137021" 
+        link: "https://www.iaaf.org/athletes/brazil/sueli-pinto-silva-137021" ,
+         tags: ""
     },
     {
         titulo: "André Ramos",
         descricao: "André Ramos é um maratonista brasileiro que se destaca no cenário nacional, com boas colocações em importantes provas do calendário brasileiro.",
-        link: "https://www.instagram.com/andreramos.run/?hl=pt-br" 
+        link: "https://www.instagram.com/andreramos.run/?hl=pt-br" ,
+         tags: ""
     }
 ];
 
